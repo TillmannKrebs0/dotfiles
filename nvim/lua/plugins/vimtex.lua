2e5147d73b979f6b879905505777cc1ed3dafe40
Tillmann Krebs <tillmannkrebs@tutamail.com>
2026-02-18 23:05:29 +0100

    glossar rendern

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,11 +1,22 @@
 vim.g.maplocalleader = " "
 
 return {
-  "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
-    -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
-  end
+    "lervag/vimtex",
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+        -- VimTeX configuration goes here, e.g.
+        vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_quickfix_mode = 1
+        vim.g.vimtex_compiler_latexmk = {
+            options = {
+                '-pdf',
+                '-shell-escape',
+                '-verbose',
+                '-file-line-error',
+                '-synctex=1',
+                '-interaction=nonstopmode',
+            },
+        }
+    end
 }
